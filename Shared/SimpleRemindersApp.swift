8eb4506fc9fd6f05f62d7679f2b2977ca185d11b
Russell Gordon <rgordon@lcs.on.ca>
2021-01-20 18:55:38 -0500

    Initial Commit

---
+++ Shared/SimpleRemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleRemindersApp.swift
+//  Shared
+//
+//  Created by Russell Gordon on 2021-01-20.
+//
+
+import SwiftUI
+
+@main
+struct SimpleRemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

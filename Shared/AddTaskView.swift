a551d9076d918a064c0817888b6923272be4de85
Russell Gordon <rgordon@lcs.on.ca>
2021-01-20 20:24:16 -0500

    Hook up the add task view to the main content view.

--- Shared/AddTaskView.swift
+++ Shared/AddTaskView.swift
@@ -13,6 +13,9 @@ struct AddTaskView: View {
     @State private var description = ""
     @State private var priority = TaskPriority.low
     
+    // Whether to show this view
+    @Binding var showing: Bool
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -28,12 +31,27 @@ struct AddTaskView: View {
                 }
             }
             .navigationTitle("New Reminder")
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button("Save") {
+                        saveTask()
+                    }
+                }
+            }
         }
+        
     }
+    
+    func saveTask() {
+        // Dismiss this view
+        showing = false
+    }
+    
+    
 }
 
 struct AddTaskView_Previews: PreviewProvider {
     static var previews: some View {
-        AddTaskView()
+        AddTaskView(showing: .constant(true))
     }
 }
--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     
+    // Stores all tasks being tracked
     @ObservedObject var store: TaskStore
+    
+    // Controls whether the add task view is showing
+    @State private var showingAddTaskView = false
     
     var body: some View {
         List(store.tasks) { task in
             TaskCell(task: task)
         }
         .navigationTitle("Reminders")
+        .toolbar {
+            ToolbarItem(placement: .primaryAction) {
+                Button("Add") {
+                    showingAddTaskView = true
+                }
+            }
+        }
+        .sheet(isPresented: $showingAddTaskView) {
+            AddTaskView(showing: $showingAddTaskView)
+        }
     }
 }
 
